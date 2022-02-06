24426e48a77077884a2cd677b56acf8c16d796f3
Vadims Sergejevs <vadims.sergejevs@gmail.com>
2022-02-06 10:11:04 +0000

    Service tests

--- test/services.test.js
+++ test/services.test.js
@@ -1,5 +1,14 @@
 // cosnt  require('../srv/services');
+const cds = require('@sap/cds/lib')
+const { GET, expect } = cds.test('serve', '--in-memory', '--project', __dirname+'/..' )
 
-test('Test something - 2+2=4', () => {
-  expect(2 + 2).toBe(4);
-});
+describe( "Basic Querying", () => {
+
+    it("should read from Catalog Products", async()=>{
+        const { data: oProduct, status } = await GET `/Products`;
+
+        expect(status).toBe(200);
+        expect(oProduct).toBeTruthy();
+    })
+
+} )
